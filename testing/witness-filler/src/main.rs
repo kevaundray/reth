12488a1c8e6440a4198b26c6e46ea9fce9733b61
Ignacio Hagopian <jsign.uy@gmail.com>
2025-08-22 17:35:43 -0300

    adjustements
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- testing/witness-filler/src/main.rs
+++ testing/witness-filler/src/main.rs
@@ -1,14 +1,18 @@
 //! execution-spec-test execution witness generator
 use std::io::{self, Read};
 
-use ef_tests::{cases::blockchain_test::run_case, models::BlockchainTest};
+use ef_tests::{cases::blockchain_test::BlockchainTestCase, models::BlockchainTest};
 
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
 
     let case: BlockchainTest = serde_json::from_str(&buffer).unwrap();
-    let witnesses = run_case(&case).unwrap().into_iter().map(|bw| bw.1).collect::<Vec<_>>();
+    let witnesses = BlockchainTestCase::run_single_case("", &case)
+        .unwrap()
+        .into_iter()
+        .map(|bw| bw.1)
+        .collect::<Vec<_>>();
 
     println!("{}", serde_json::to_string(&witnesses).unwrap());
 }
