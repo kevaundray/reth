34fb8bdc1898427226d042c5bb33dcecf2e7045f
Ignacio Hagopian <jsign.uy@gmail.com>
2025-10-23 20:08:47 -0300

    cleanup

--- testing/runner/src/main.rs
+++ testing/runner/src/main.rs
@@ -1,5 +1,5 @@
 //! Command-line interface for running tests.
-use std::{path::PathBuf, str::FromStr};
+use std::path::PathBuf;
 
 use clap::Parser;
 use ef_tests::{cases::blockchain_test::BlockchainTests, Suite};
@@ -12,9 +12,6 @@ pub struct TestRunnerCommand {
 }
 
 fn main() {
-    // let mut cmd = TestRunnerCommand::parse();
-    let suite_path =
-        // PathBuf::from_str("/data/code-data/kev-reth/testing/ef-tests/reth-debug").unwrap();
-        PathBuf::from_str("/data/code-data/kev-reth/testing/ef-tests/execution-spec-tests").unwrap();
-    BlockchainTests::new(suite_path.join("blockchain_tests")).run();
+    let cmd = TestRunnerCommand::parse();
+    BlockchainTests::new(cmd.suite_path.join("blockchain_tests")).run();
 }
